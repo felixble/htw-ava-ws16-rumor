14b8617000ab16befb0076a7938ce937eee8bd7d
felix <felix@9fb895f6-f155-4ec7-9ba7-def33b723103>
2016-10-30 20:53:16 +0000

    init-Skript mit dialog zum Senden von Kontrollnachrichten / Stoppen des Servers über Kontrollnachricht

--- trunk/rumor/src/init.js
+++ trunk/rumor/src/init.js
@@ -1,11 +1,51 @@
 
 import { Client } from './lib/client';
+let readLine = require('./lib/read-line');
+
+async function initClient(host, port) {
+    let client = new Client(host, port);
+    await client.connect();
+    await client.send({msg: 'INIT'});
+    client.close();
+}
+
+async function stopClient(host, port) {
+    let client = new Client(host, port);
+    await client.connect();
+    await client.send({msg: 'STOP'});
+    client.close();
+}
 
 async function main() {
-    let client = new Client('127.0.0.1', 6000);
-    await client.connect();
-    await client.send({msg: 'init'});
-    client.close();
+    let exit = false;
+    while(!exit) {
+        let cmd = await readLine('Enter command:');
+        switch (cmd) {
+            case 'init':
+            {
+                let host = await readLine('Enter host:');
+                let port = await readLine('Enter port:');
+                await initClient(host, port);
+                break;
+            }
+            case 'stop':
+            {
+                let host = await readLine('Enter host:');
+                let port = await readLine('Enter port:');
+                await stopClient(host, port);
+                break;
+            }
+            case 'exit':
+            {
+                exit = true;
+                break;
+            }
+            default: {
+                console.log('Unknown command: ' + cmd);
+            }
+        }
+    }
+    process.exit();
 }
 
 main();
