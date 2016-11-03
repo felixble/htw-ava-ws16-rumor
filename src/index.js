02aa530ff297bed6795b695e087029c916acc753
felix <felix@9fb895f6-f155-4ec7-9ba7-def33b723103>
2016-11-03 17:46:12 +0000

    main program parameters
    - Read command line parameters to set options

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import { EndpointManager } from './endpointManager'
 
 let readLine = require('./lib/read-line');
 
-const endpointFilename = './config/endpoints';
-const graphFilename = './config/graph.dot';
+let optionParser = require('node-getopt').create([
+    ['e', 'endpointFilename=[ARG]', 'path to the endpoints file'],
+    ['g', 'graphFilename=[ARG]', 'path to the graph file defining the network node topology'],
+    ['id', 'id=[ARG]', 'ID of this endpoint'],
+    ['h', 'help', 'Display this help']
+]);
+
+let arg = optionParser.bindHelp().parseSystem();
+
+const endpointFilename = arg.options.endpointFilename || './config/endpoints';
+const graphFilename = arg.options.graphFilename || './config/graph.dot';
 
 
 async function main() {
     let endpointManager = new EndpointManager(endpointFilename, graphFilename);
     await endpointManager.init();
-    let myId = await readLine('Please insert the ID of this endpoint:');
+    let myId = arg.options.id || await readLine('Please insert the ID of this endpoint:');
     await endpointManager.setMyId(myId);
     console.log(endpointManager.getMyEndpoint());
     let myServer = new Server(endpointManager.getMyEndpoint().host, endpointManager.getMyEndpoint().port);
